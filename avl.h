5eafe84b2bbb52eec745b9cd70802ede9d6d0dc1
Andy00006 <andy.laloum8@gmail.com>
2025-12-20 12:40:53 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-// Structure représentant une usine dans l'arbre équilibré
+// Structure AVL (pour Histo et recherche rapide)
 typedef struct Noeud {
     char identifiant[100];
     double capacite_max;
@@ -14,8 +14,18 @@ typedef struct Noeud {
     struct Noeud *droit;
 } Noeud;
 
-// Prototypes des fonctions de gestion de l'arbre
+// Structure pour l'Arbre de Distribution (Mode Leaks)
+typedef struct NoeudDist {
+    char id[100];
+    double pct_fuite;
+    struct NoeudDist** enfants;
+    int nb_enfants;
+    int capacite_tab;
+} NoeudDist;
+
+// Prototypes
 Noeud* inserer(Noeud* noeud, char* id, double capa, double capt, double trait);
+Noeud* rechercher_noeud(Noeud* racine, char* id);
 void liberer_arbre(Noeud* racine);
 void exporter_infixe_inverse(Noeud* racine, FILE* flux_sortie);
 
